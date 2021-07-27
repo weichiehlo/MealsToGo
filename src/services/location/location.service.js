fc03e648b0118a0590d593c2a850f0a6f285afb4
WEI CHIEH LO <weichiehlo@gmail.com>
2021-07-27 13:07:52 -0700

    migrating restaurant mock data to firebase function

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -2,17 +2,15 @@ import camelize from "camelize";
 
 export const locationRequest = (searchTerm) => {
   return fetch(
-    `http://5c96a05037ce.ngrok.io/mealstogo-d1a74/us-central1/geocode?city=${searchTerm}`
+    `http://b40520e960d3.ngrok.io/mealstogo-d1a74/us-central1/geocode?city=${searchTerm}`
   ).then((res) => {
     return res.json();
   });
 };
 
 export const locationTransform = (result) => {
-  console.log(result);
   const formatedResponse = camelize(result);
   const { geometry = {} } = formatedResponse.results[0];
   const { lat, lng } = geometry.location;
   return { lat, lng, viewport: geometry.viewport };
 };
-
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,20 +1,14 @@
-import { mocks, mockImages } from "./mock/index";
 import camelize from "camelize";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject("not found");
-    }
-    resolve(mock);
+  return fetch(
+    `http://b40520e960d3.ngrok.io/mealstogo-d1a74/us-central1/placesNearby?location=${location}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
-export const restaurantsTransform = ({ results = [] }) => {
+  export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
     return {
       ...restaurant,
       IsOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
